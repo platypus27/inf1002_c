f04e033e9c069ef26d1b609b19a7f11d014dc966
jjjjjaayyyyyyyyyyy <jjay05lim@gmail.com>
2024-03-26 21:09:34 +0800

    added function to check if generated folder exists

--- src/export.c
+++ src/export.c
@@ -11,6 +11,7 @@
 #define SORT_C
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 #include "../include/config.h"
 #include "../include/functions.h"
 
@@ -89,6 +90,22 @@ void generateChart(char title[], char categories[][MAX_NAME_LEN + 1], int quanti
     tickSpacing = maxScaledQty / (numTickMarks - 1);
 }
 
+int generatedFolder() {
+    char* folderPath = "generated/";
+    DWORD dwAttrib = GetFileAttributes(folderPath);
+    if (dwAttrib != INVALID_FILE_ATTRIBUTES && (dwAttrib & FILE_ATTRIBUTE_DIRECTORY)) {
+        return 1;
+    } else {
+        if (!CreateDirectory(folderPath, NULL)) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
 /**
  * Function to export the chart to a file
  * File path can be found in config file
@@ -101,59 +118,63 @@ void generateChart(char title[], char categories[][MAX_NAME_LEN + 1], int quanti
  * @return void
 */
 void exportChart(char *title, char categories[][MAX_NAME_LEN + 1], int quantities[], int numCategories, int scaleofXaxis, char *xAxisLabel) {
-    char input[30];
-    char filePath[] = "generated/";
-    printf("\nEnter file path to save to: ");
-    scanf(" %[^\n]", input);    
-    strcat(filePath, input);
-    strcat(filePath, ".txt");
+    if (generatedFolder()) {
+        char input[30];
+        char filePath[] = "generated/";
+        printf("\nEnter file path to save to: ");
+        scanf(" %[^\n]", input);    
+        strcat(filePath, input);
+        strcat(filePath, ".txt");
 
-    // Open a file for writing
-    FILE *file = fopen(filePath, "a");
-    if (file == NULL) {
+        // Open a file for writing
+        FILE *file = fopen(filePath, "a");
+        if (file == NULL) {
+            printf("%s",FILE_ERROR);
+        }
+
+        // Write the chart to the file
+        int maxQuantity = 0;
+        int frontSpacing = 0;
+
+        for (int i = 0; i < numCategories; i++) {
+            // Find maximum quantity for scaling
+            if (quantities[i] > maxQuantity) {
+                maxQuantity = quantities[i];
+            }
+            // Get largest length of front spacing
+            if (strlen(categories[i]) > frontSpacing) {
+                frontSpacing = strlen(categories[i]);
+            }
+        }
+
+        scaleofXaxis = multiplier(maxQuantity);
+
+        // Find maximum scaled quantity for x-axis scaling
+        int maxScaledQty = 0;
+        for (int i = 0; i < numCategories; i++)
+        {
+            int scaledQty = (quantities[i] * 60) / maxQuantity;
+            if (scaledQty > maxScaledQty)
+            {
+                maxScaledQty = scaledQty;
+            }
+        }
+
+
+        generateChart(title, categories, quantities, numCategories, maxQuantity, maxScaledQty, frontSpacing, file);
+        generateX(maxQuantity, scaleofXaxis, maxScaledQty, frontSpacing, tickSpacing, file);
+
+        // print x-axis label
+        xAxisLabel = alignCenter(xAxisLabel, maxScaledQty + frontSpacing);
+        fprintf(file, "\n%s(x%d)", xAxisLabel, scaleofXaxis);
+
+        // Close the file
+        fclose(file);
+
+        printf("%s", SUCCESS_SAVE);
+    } else {
         printf("%s",FILE_ERROR);
     }
-
-    // Write the chart to the file
-    int maxQuantity = 0;
-    int frontSpacing = 0;
-
-    for (int i = 0; i < numCategories; i++) {
-        // Find maximum quantity for scaling
-        if (quantities[i] > maxQuantity) {
-            maxQuantity = quantities[i];
-        }
-        // Get largest length of front spacing
-        if (strlen(categories[i]) > frontSpacing) {
-            frontSpacing = strlen(categories[i]);
-        }
-    }
-
-    scaleofXaxis = multiplier(maxQuantity);
-
-    // Find maximum scaled quantity for x-axis scaling
-    int maxScaledQty = 0;
-    for (int i = 0; i < numCategories; i++)
-    {
-        int scaledQty = (quantities[i] * 60) / maxQuantity;
-        if (scaledQty > maxScaledQty)
-        {
-            maxScaledQty = scaledQty;
-        }
-    }
-
-
-    generateChart(title, categories, quantities, numCategories, maxQuantity, maxScaledQty, frontSpacing, file);
-    generateX(maxQuantity, scaleofXaxis, maxScaledQty, frontSpacing, tickSpacing, file);
-
-    // print x-axis label
-    xAxisLabel = alignCenter(xAxisLabel, maxScaledQty + frontSpacing);
-    fprintf(file, "\n%s(x%d)", xAxisLabel, scaleofXaxis);
-
-    // Close the file
-    fclose(file);
-
-    printf("%s", SUCCESS_SAVE);
 }
 
 /**
@@ -166,37 +187,41 @@ void exportChart(char *title, char categories[][MAX_NAME_LEN + 1], int quantitie
  * @return int
 */
 void exportData(char *title, char categories[][MAX_NAME_LEN + 1], int quantities[], int numCategories, int scaleofXaxis, char *xAxisLabel){
-    char filePath[155];
-    // Ask for input for the file name
-    char fileName[100];
-    printf("Enter file name: ");
-    scanf("%s", fileName);
+    if (generatedFolder()) {
+        char filePath[155];
+        // Ask for input for the file name
+        char fileName[100];
+        printf("Enter file name: ");
+        scanf("%s", fileName);
 
-    sprintf(filePath, "%s%s.csv", DIR_PATH, fileName);
-    FILE *fileExists = fopen(filePath, "r");
-    if (fileExists != NULL) {
-        printf("File with the same name already exists.\n");
-        fclose(fileExists);
-        return; // or handle the situation accordingly
-    }
+        sprintf(filePath, "%s%s.csv", DIR_PATH, fileName);
+        FILE *fileExists = fopen(filePath, "r");
+        if (fileExists != NULL) {
+            printf("File with the same name already exists.\n");
+            fclose(fileExists);
+            return; // or handle the situation accordingly
+        }
 
-    // Open a file for writing
-    FILE *file = fopen(filePath, "w");
-    if (file == NULL) {
-        printf("Failed to open file\n");
+        // Open a file for writing
+        FILE *file = fopen(filePath, "w");
+        if (file == NULL) {
+            printf("Failed to open file\n");
+            printf("%s", FILE_ERROR);
+            return; // or handle the situation accordingly
+        }
+
+        // Write the data to the file
+        fprintf(file, "title,xAxisLabel\n");
+        for (int i = 0; i < numCategories; i++) {
+            fprintf(file, "%s,%d\n", categories[i], quantities[i]);
+        }
+
+        // Close the file
+        fclose(file);
+
+        printf("%s %s%s\n", SUCCESS_SAVE_DATA, DIR_PATH, fileName);
+    } else {
         printf("%s", FILE_ERROR);
-        return; // or handle the situation accordingly
     }
-
-    // Write the data to the file
-    fprintf(file, "title,xAxisLabel\n");
-    for (int i = 0; i < numCategories; i++) {
-        fprintf(file, "%s,%d\n", categories[i], quantities[i]);
-    }
-
-    // Close the file
-    fclose(file);
-
-    printf("%s %s%s\n", SUCCESS_SAVE_DATA, DIR_PATH, fileName);
 }
 #endif
